22b9062890270d4771631fe98bfdc9bff5b9f815
KIrillPrushak <kirillprushak@gmail.com>
2025-07-30 22:55:52 +0300

    add component Action in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import Action from "@/components/Action";
+import ProductCard from "@/components/ProductCard";
 import Slider from "@/components/slider/Slider";
 
 export default function Home() {
   return (
     <main className="w-full mx-auto mb-20">
       <Slider />
+      <div
+        className="px-[max(12px,calc((100%-1208px)/2))]
+          flex flex-col gap-y-20 md:mb-25 xl:mb-30"
+      >
+        <Action />
+      </div>
     </main>
   );
 }
